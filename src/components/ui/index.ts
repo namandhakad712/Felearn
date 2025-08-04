d686ef5fd99938233d857fbfb82d598b4774428d
namandhakad712 <namandhakad712@gmail.com>
2025-08-04 19:05:33 +0530

    CH

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,7 +2,7 @@ export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Modal } from './Modal';
 export { default as Spinner } from './Spinner';
-export { default as Toast } from './Toast';
+export { default as Toast, ToastContainer } from './Toast';
 export { default as ThemeToggle } from './ThemeToggle';
 export { default as Carousel } from './Carousel';
 export { default as JellyText } from './JellyText';
